74d8c25ac9b554e6ebbc6a7adbc18d665aa24d58
Andrew Wever <andrew.wever@gmail.com>
2022-11-08 11:51:10 -0500

    add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # Show all
   get 'tasks', to: 'tasks#index'
 
+  # Add one
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
   # Show one
   get "tasks/:id", to: "tasks#show", as: :task
 end
