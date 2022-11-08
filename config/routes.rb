fce8c47155c2cd1182a055011ad723fee14fee27
Andrew Wever <andrew.wever@gmail.com>
2022-11-08 13:32:52 -0500

    add, edit, and delete tasks

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
 
   # Show one
   get "tasks/:id", to: "tasks#show", as: :task
+
+  # Edit one
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete one
+  delete "tasks/:id", to: "tasks#destroy"
 end
